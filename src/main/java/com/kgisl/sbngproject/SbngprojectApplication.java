5fa73ebe871d35dbf27f98de3ae4be7e2269e81f
Swathi3801 <101704982+Swathi3801@users.noreply.github.com>
2024-02-02 14:49:00 +0530

    Update SbngprojectApplication.java

--- src/main/java/com/kgisl/sbngproject/SbngprojectApplication.java
+++ src/main/java/com/kgisl/sbngproject/SbngprojectApplication.java
@@ -2,20 +2,26 @@ package com.kgisl.sbngproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Configuration
+public class SbngprojectApplication implements WebMvcConfigurer {
 
-public class SbngprojectApplication implements WebMvcConfigurer{
-	String baseUrl = System.getenv("https://sbwithng.onrender.com/");
-    if (baseUrl == null) {
-      baseUrl = "http://localhost:8080"; // use localhost as default
+    public static void main(String[] args) {
+        SpringApplication.run(SbngprojectApplication.class, args);
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(SbngprojectApplication.class, args);
-	}
+    @Bean
+    public String baseUrl() {
+        String baseUrl = System.getenv("https://sbwithng.onrender.com/");
 
+        if (baseUrl == null) {
+            baseUrl = "http://localhost:8080";
+        }
+
+        return baseUrl;
+    }
 }
