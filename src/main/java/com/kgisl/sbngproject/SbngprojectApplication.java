cdffb3422e0dca65d1853286011eba8bc2feb02f
Swathi3801 <101704982+Swathi3801@users.noreply.github.com>
2024-02-02 16:05:22 +0530

    Update SbngprojectApplication.java

--- src/main/java/com/kgisl/sbngproject/SbngprojectApplication.java
+++ src/main/java/com/kgisl/sbngproject/SbngprojectApplication.java
@@ -1,25 +1,17 @@
 package com.kgisl.sbngproject;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @Configuration
-public class SbngprojectApplication implements WebMvcConfigurer {
 
-    @Value("${WORKFLOW_URL:http://localhost:8080}")
-    private String baseUrl;
+public class SbngprojectApplication implements WebMvcConfigurer{
 
-    public static void main(String[] args) {
-        SpringApplication.run(SbngprojectApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SbngprojectApplication.class, args);
+	}
 
-    @Bean
-    public String baseUrl() {
-        return baseUrl;
-    }
 }
